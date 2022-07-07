9a11f48c4f0813ecd7296d5ba4c8b9f92aa0e8be
Magomed Ilyasov <mkznworkflow@gmail.com>
2022-07-07 11:23:49 +0300

    added task2

--- tasks/basic-module.js
+++ tasks/basic-module.js
@@ -1,4 +1,7 @@
 // Escape Sequences in Strings
 
-const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+const myStr1 = "FirstLine\n\t\\SecondLine\nThirdLine";
 
+// Concatenating Strings with Plus Operator
+
+const myStr2 = "This is the start. " + "This is the end";
