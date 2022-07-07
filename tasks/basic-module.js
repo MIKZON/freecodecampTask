017a5b52ce2d3de58b821ef1868d2e688bc46d18
Magomed Ilyasov <mkznworkflow@gmail.com>
2022-07-07 11:30:08 +0300

    added tasks

--- tasks/basic-module.js
+++ tasks/basic-module.js
@@ -1,7 +1,23 @@
 // Escape Sequences in Strings
-
 const myStr1 = "FirstLine\n\t\\SecondLine\nThirdLine";
 
-// Concatenating Strings with Plus Operator
 
+// Concatenating Strings with Plus Operator
 const myStr2 = "This is the start. " + "This is the end";
+
+
+//Concatenating Strings with the Plus Equals Operator
+let myStr3 = 'This is the first sentence. ';
+myStr3 += 'This is the second sentence.'
+
+
+//Constructing Strings with Variables
+const myName = "hidden";
+const myStr4 = "Hi " + myName + "how are u?";
+
+//Appending Variables to Strings
+const someAdjective = "Awesome!";
+let myStr5 = "Learning to code is ";
+myStr += someAdjective
+
+
