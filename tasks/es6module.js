d6a1241a5f33380b6b08eec18c26b5fe3402c2bd
Magomed Ilyasov <mkznworkflow@gmail.com>
2022-07-12 15:31:59 +0300

    added promise tasks

--- tasks/es6module.js
+++ tasks/es6module.js
@@ -148,4 +148,89 @@ const createPerson = (name, age, gender) => {
 
 
 //Write Concise Declarative Functions with ES6
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+bicycle.setGear(3);
+console.log(bicycle.gear);
 
+
+//Use class Syntax to Define a Constructor Function
+class Vegetable {
+  constructor(name) {
+     return this.name = name
+  }
+}
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
+
+
+//Use getters and setters to Control Access to an Object
+class Thermostat {
+  constructor(fahrenheit) {
+    this.fahrenheit = fahrenheit;
+  }
+  get temperature() {
+    return (5 / 9) * (this.fahrenheit - 32);
+  }
+  set temperature(celsius) {
+    this.fahrenheit = (celsius * 9.0) / 5 + 32;
+  }
+}
+
+
+//Create a Module Script
+<html>
+  <body>
+  <script type="module" src="index.js"></script>
+  </body>
+</html>
+
+
+//Use export to Share a Code Block
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+
+export  { uppercaseString, lowercaseString }
+
+
+//Reuse JavaScript Code Using import
+import  { uppercaseString, lowercaseString }  from './string_functions.js'
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
+
+
+//Use * to Import Everything from a File
+import * as stringFunctions from './string_functions.js'
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
+
+//Create an Export Fallback with export default
+export default function subtract(x, y) {
+  return x - y;
+}
+
+
+//Import a Default Export
+import subtract from './math_functions.js'
+subtract(7,4);
+
+
+//Create a JavaScript Promise
+const makeServerRequest = new Promise((resolve, reject) => {
+})
+
+
+//Complete a Promise with resolve and reject
