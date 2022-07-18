de375905b9a7588d85a5c3e9387534386ebb75be
Magomed Ilyasov <mkznworkflow@gmail.com>
2022-07-18 23:45:43 +0300

    added tasks for oop

--- tasks/object-o-programming.js
+++ tasks/object-o-programming.js
@@ -164,3 +164,128 @@ function Dog(name) {
 }
 let beagle = new Dog("Snoopy");
 Dog.prototype.isPrototypeOf(beagle)
+
+
+//Understand the Prototype Chain
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+Dog.prototype.isPrototypeOf(beagle);
+Object.prototype.isPrototypeOf(Dog.prototype);
+
+
+//Use Inheritance So You Don't Repeat Yourself
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+
+};
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+    eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+
+//Inherit Behaviors from a Supertype
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+let duck = Object.create(Animal.prototype);
+let beagle = Object.create(Animal.prototype);
+
+
+//Set the Child's Prototype to an Instance of the Parent
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+Dog.prototype = Object.create(Animal.prototype)
+let beagle = new Dog();
+
+
+//Reset an Inherited Constructor Property
+function Animal() { }
+function Bird() { }
+function Dog() { }
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+let duck = new Bird();
+Bird.prototype.constructor = Bird;
+let beagle = new Dog();
+Dog.prototype.constructor = Dog;
+
+
+//Add Methods After Inheritance
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+function Dog() { }
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function () {
+  console.log('Woof!')
+}
+let beagle = new Dog();
+
+
+//Override Inherited Methods
+function Bird() { }
+Bird.prototype.fly = function() { return "I am flying!"; };
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+Penguin.prototype.fly = function () {
+  return 'Alas, this is a flightless bird.';
+}
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+
+
+//Use a Mixin to Add Common Behavior Between Unrelated Objects
+let bird = {
+  name: "Donald",
+  numLegs: 2
+};
+let boat = {
+  name: "Warrior",
+  type: "race-boat"
+};
+let glideMixin = function(obj) {
+  obj.glide = function() {
+    console.log("Flying, wooosh!");
+  }
+};
+glideMixin(bird);
+glideMixin(boat);
+
+
+//Use Closure to Protect Properties Within an Object from Being Modified Externally
