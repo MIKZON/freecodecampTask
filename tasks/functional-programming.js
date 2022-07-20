6559f4e48a92968e2f1f7fb83583fec16ca8f29b
Magomed Ilyasov <mkznworkflow@gmail.com>
2022-07-20 15:27:08 +0300

    added functional tasks

--- tasks/functional-programming.js
+++ tasks/functional-programming.js
@@ -32,3 +32,46 @@ console.log(
   tea4GreenTeamFCC,
   tea4BlackTeamFCC
 );
+
+
+//Understand the Hazards of Using Imperative Code
+const Window = function(tabs) {
+  this.tabs = tabs;
+};
+Window.prototype.join = function(otherWindow) {
+  this.tabs = this.tabs.concat(otherWindow.tabs);
+  return this;
+};
+
+Window.prototype.tabOpen = function(tab) {
+  this.tabs.push('new tab');
+  return this;
+};
+
+Window.prototype.tabClose = function(index) {
+  const tabsBeforeIndex = this.tabs.splice(0, index);
+  const tabsAfterIndex = this.tabs.splice(1);
+
+  this.tabs = tabsBeforeIndex.concat(tabsAfterIndex);
+  return this;
+ };
+
+const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
+
+const finalTabs = socialWindow
+  .tabOpen()
+  .join(videoWindow.tabClose(2))
+  .join(workWindow.tabClose(1).tabOpen());
+console.log(finalTabs.tabs);
+
+
+
+//Avoid Mutations and Side Effects Using Functional Programming
+let fixedValue = 4;
+
+function incrementer() {
+let fi = fixedValue + 1;
+  return fi;
+}
