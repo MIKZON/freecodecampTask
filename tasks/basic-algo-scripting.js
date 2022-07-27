0cc145779197799259c56f046f8c7f3015f3606c
Magomed Ilyasov <mkznworkflow@gmail.com>
2022-07-27 15:32:42 +0300

    finished regexp

--- tasks/basic-algo-scripting.js
+++ tasks/basic-algo-scripting.js
@@ -153,3 +153,9 @@ let str = "one two three";
 let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
 let replaceText = "$3 $2 $1"; // Change this line
 let result = str.replace(fixRegex, replaceText);
+
+
+//Remove Whitespace from Start and End
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g;
+let result = hello.replace(wsRegex, "");
